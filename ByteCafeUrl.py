c82f66419bb2ad840d599be7e866fde04a4089d1
Alex Cordero <alex.cordero@acordero-01.workdayinternal.com>
2016-12-01 15:43:33 -0800

    byteCafeUrl function

---
+++ ByteCafeUrl.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+
+def byteCafeUrl():
+    today = datetime.today() - timedelta(days = 18)
+    weekday = today.weekday()
+    last_monday = today - timedelta(days=weekday)
+    monday_month = str(last_monday.month)
+    monday_day = str(last_monday.day)
+    monday_year = str(last_monday.year)
+    if int(monday_day) < 10:
+        monday_day = "0" + monday_day
+    if int(monday_month) < 10:
+        monday_day = "0" + monday_month
+
+    byte_url = "http://dining.guckenheimer.com/clients/npcholdings/fss/fss.nsf/" + \
+                "weeklyMenuLaunch/9W4S24~" + monday_month + "-" + monday_day + \
+                "-" + monday_year + "/%24file/cafehome.htm"
+    return byte_url
